05a8ccc8ff50507b85daa9858cc6cb4063495bec
Avneh Singh Bhatia <98300720+RedMythic1@users.noreply.github.com>
2023-04-28 01:32:30 +0000

    g

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import finnhub
-import pandas as pd
-import plotly.express as px
+
 
 fh = finnhub.Client(api_key="cakj47iad3ier73m8dtg")
-BTC = fh.quote("BTC-USD")
+AAPL = fh.quote("AAPL")
 NVDA = fh.quote("NVDA")
 
-BTC_GAIN = BTC['c'] - BTC['pc']
-NVDA_EST_GAIN = round(BTC_GAIN*(93/10000),2)
-NVDA_EST_CLOSE = round(BTC['c']*(93/10000),2)
+AAPL_GAIN = AAPL['c'] - AAPL['pc']
+NVDA_EST_CLOSE = round(-38.6718*((1)**((AAPL['c']**6)-20900000000000))+314.034,2)
 
 NVDA_ACT_CLOSE = round(NVDA['c'],2)
-NVDA_ACT_GAIN = round(NVDA['c'] - NVDA['pc'],2)
 
-NVDA_GAIN_DEV = round(NVDA_EST_GAIN/NVDA_ACT_GAIN,2)
 NVDA_CLOSE_DEV = round(NVDA_EST_CLOSE/NVDA_ACT_CLOSE,2)
-print(NVDA_EST_CLOSE,NVDA_EST_GAIN,NVDA_ACT_CLOSE,NVDA_ACT_GAIN, NVDA_CLOSE_DEV, NVDA_GAIN_DEV)
+print(NVDA_EST_CLOSE,NVDA_ACT_CLOSE, NVDA_CLOSE_DEV)
