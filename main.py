4d4fd58cb78abea5e6f8a2cd3bc8fff9c2c73fb3
Avneh Singh Bhatia <98300720+RedMythic1@users.noreply.github.com>
2023-04-28 02:07:29 +0000

    yesir

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ fh = finnhub.Client(api_key="cakj47iad3ier73m8dtg")
 AAPL = fh.quote("AAPL")
 NVDA = fh.quote("NVDA")
 
+
 AAPL_GAIN = AAPL['c'] - AAPL['pc']
 NVDA_EST_CLOSE = round(-38.6718*((1)**((AAPL['c']**6)-20900000000000))+314.034,2)
 
 NVDA_ACT_CLOSE = round(NVDA['c'],2)
 
-NVDA_CLOSE_DEV = round(NVDA_EST_CLOSE/NVDA_ACT_CLOSE,2)
+NVDA_CLOSE_DEV = round(NVDA_EST_CLOSE/NVDA_ACT_CLOSE,4)
 print(NVDA_EST_CLOSE,NVDA_ACT_CLOSE, NVDA_CLOSE_DEV)
